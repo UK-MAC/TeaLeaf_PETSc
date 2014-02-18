0254a27aa6afc255efc60adebbb09d1efdc06bfb
Wayne Gaudin <wpg@pan.hpsg.dcs.warwick.ac.uk>
2014-02-18 15:31:46 +0000

    prefix fix

--- build-petsc.sh
+++ build-petsc.sh
@@ -5,7 +5,7 @@ BLAS_DIR=
 MPI_DIR=
 PREFIX=.
 
-./configure --with-prefix=${PREFIX} --with-c++-support=1 --with-c-support=1 \
+./configure --prefix=${PREFIX} --with-c++-support=1 --with-c-support=1 \
   --with-fortran=1 --with-x11=no --with-mpi=1 --with-hypre=0 --with=spooles=0 \
   --with-ml=0 --with-mpi-dir=${MPI_DIR} --with-blas-lapack-dir=${BLAS_DIR}/lib \
   --with-debugging=no PETSC_ARCH=linux-static
