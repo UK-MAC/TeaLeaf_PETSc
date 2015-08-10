fad582fb1de6f2fe73091a61e94a692e6300b69a
James Southern <jsouthern@sgi.com>
2015-08-10 05:05:00 -0500

    Add sample build script for petsc-dev.
    Note that this will need to be modified when building on different systems.

---
+++ petsc-dev/build-petsc.sh
@@ -0,0 +1,25 @@
+export LANG=C
+export LC_ALL=C
+
+# How you select a petsc compiler
+export I_MPI_CC="icc -lmpi"
+export I_MPI_CXX="icpc -lmpi"
+export I_MPI_F77="ifort -lmpi"
+export I_MPI_F90="ifort -lmpi"
+
+BLAS_FLAGS="-mkl=sequential"
+MPI_DIR=/sw/sdev/mpt-x86_64/2.12-p11218
+PREFIX=/store/jsouthern/packages/petsc/dev
+
+./configure --prefix=${PREFIX} --with-c++-support=1 --with-c-support=1 \
+  --with-cc=${I_MPI_CC} --with-cxx=${I_MPI_CXX} --with-fc=${I_MPI_F77} \
+  --with-fortran=1 --with-x11=no --with-mpi=1 --with-hypre=0 --with=spooles=0 \
+  --with-ml=0 --with-mpiexec=mpirun --with-blas-lapack-lib=${BLAS_FLAGS} \
+  --with-debugging=no PETSC_ARCH=linux-static --with-shared-libraries=0 \
+  --with-x=0 --with-threadcomm=1 --with-openmp=1 --with-pthreadclasses=1
+
+export PETSC_ARCH=linux-static
+export PETSC_DIR=${PWD}
+
+make all
+make install
